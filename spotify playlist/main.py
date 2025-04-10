97b70e6f1a1c5118a526d0ebb40b4aafe9bfad01
dhiva <dhivakar13092000@gmail.com>
2025-04-10 23:50:29 +0530

    link updated

--- spotify playlist/main.py
+++ spotify playlist/main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+# https://www.billboard.com/charts/hot-100/2000-08-12
+
 travel = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD: ")
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
